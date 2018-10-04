855172b24c664bcd7087d86ef9ba225002cff450
andy <andnpatterson@gmail.com>
2018-10-03 18:51:38 -0600

    copy over matrix utilities and convert tests to jest

---
+++ src/Matrix.ts
@@ -0,0 +1,198 @@
+export type Dim = {rows: number, cols: number};
+export type BufferConstructor = Uint8ArrayConstructor | Float32ArrayConstructor | Int32ArrayConstructor;
+export type BufferType = Uint8Array | Float32Array | Int32Array;
+
+export default class Matrix<B extends BufferConstructor = Float32ArrayConstructor> {
+    private data: BufferType;
+    private transposed: boolean = false;
+
+    static Zeros(dim: Dim) {
+        return new Matrix(Float32Array, dim).fill(0);
+    };
+
+    static fromMatrix(m: Matrix<any>) {
+        const dims = m.dims();
+        const Buffer = m.Buffer;
+
+        return new Matrix(Buffer, dims).fill((i, j) => m.get(i, j));
+    };
+
+    static fromBuffer<B extends Buffer>(buffer: B, dim: Dim) {
+        if (buffer instanceof Float32Array) {
+            return new Matrix(Float32Array, dim).load(buffer);
+        } else if (buffer instanceof Int32Array) {
+            return new Matrix(Int32Array, dim).load(buffer);
+        } else {
+            return new Matrix(Uint8Array, dim).load(buffer);
+        }
+    }
+
+    static fromData<B extends BufferConstructor = Float32ArrayConstructor>(data: number[][], Buf?: B) {
+        const dims: Dim = { rows: data.length, cols: data[0].length };
+        const Buffer = Buf || Float32Array;
+        return new Matrix(Buffer, dims).fill((i, j) => data[i][j]);
+    }
+
+    constructor(private Buffer: B, private dim: Dim, buffer?: InstanceType<B>) {
+        if (buffer && buffer.length !== dim.cols * dim.rows) throw new Error(`Expected provided data to match provided dimensions. Expected <${dim.rows * dim.cols}>, got <${buffer.length}>`);
+        this.data = buffer || new Buffer(dim.cols * dim.rows);
+    };
+
+    inBounds(a: number, b: number) {
+        const { rows, cols } = this.dims();
+        return  a >= 0 &&
+                b >= 0 &&
+                a < rows &&
+                b < cols;
+    };
+
+    private boundaryCheck(a: number, b: number) {
+        const { rows, cols } = this.dims();
+        if (!this.inBounds(a, b))
+            throw new Error(`Out-of-bounds: (${a}, ${b}) is out of bounds for (${rows}, ${cols}) matrix`);
+    };
+
+    private merge(m: Matrix<B>) {
+        this.transposed = m.transposed;
+        this.data = m.data;
+        this.dim = m.dim;
+        return this;
+    }
+
+    get raw() { return this.data; }
+    get rows() { return this.dims().rows; }
+    get cols() { return this.dims().cols; }
+
+    get(a: number, b: number) {
+        this.boundaryCheck(a, b);
+        if (this.transposed) return this.data[b * this.rows + a];
+        return this.data[a * this.cols + b];
+    };
+
+    set(a: number, b: number, v: number) {
+        this.boundaryCheck(a, b);
+        if (this.transposed) this.data[b * this.rows + a] = v;
+        else this.data[a * this.cols + b] = v;
+        return this;
+    };
+
+    load(data: InstanceType<B>) {
+        if (data.length !== this.cols * this.rows) throw new Error(`Loaded data invalid length. got: <${data.length}>, expected: <${this.cols * this.rows}>`);
+        this.data = data;
+        return this;
+    };
+
+    fill(f: ((i: number, j: number) => number) | number) {
+        const g = typeof f === 'function' ? f : () => f;
+
+        for (let i = 0; i < this.rows; ++i) {
+            for (let j = 0; j < this.cols; ++j) {
+                this.set(i, j, g(i, j));
+            }
+        }
+
+        return this;
+    }
+
+    dims() {
+        const dim1 = this.dim.rows;
+        const dim2 = this.dim.cols;
+        return {
+            rows: this.transposed ? dim2 : dim1,
+            cols: this.transposed ? dim1 : dim2
+        };
+    };
+
+    transpose() {
+        this.transposed = !this.transposed;
+    };
+
+    addRow(data: Array<number>) {
+        const { cols } = this.dims();
+        if (data.length !== cols) throw new Error(`Row of length: ${data.length} does not match matrix cols: ${cols}`);
+
+        const m = new Matrix(this.Buffer, { rows: this.rows + 1, cols: this.cols });
+        m.transposed = this.transposed
+        m.fill((i, j) => {
+            if (this.inBounds(i, j)) return this.get(i, j);
+            return data[j];
+        });
+
+        this.merge(m);
+    };
+
+    addCol(data: Array<number>) {
+        const { rows } = this.dims();
+        if (data.length !== rows) throw new Error(`Col of length: ${data.length} does not match matrix rows: ${rows}`);
+
+        const m = new Matrix(this.Buffer, { rows: this.rows, cols: this.cols + 1 });
+        m.transposed = this.transposed;
+        m.fill((i, j) => {
+            if (this.inBounds(i, j)) return this.get(i, j);
+            return data[i];
+        });
+
+        this.merge(m);
+    };
+
+    getRow(i: number) {
+        this.boundaryCheck(i, 0);
+        const row = [];
+        for (let j = 0; j < this.cols; ++j) {
+            row.push(this.get(i, j));
+        }
+        return row;
+    };
+
+    getCol(i: number) {
+        this.boundaryCheck(0, i);
+        const ret = [];
+        for (let j = 0; j < this.rows; ++j) {
+            ret.push(this.get(j, i));
+        }
+        return ret;
+    };
+
+    asArrays() {
+        const ret: number[][] = [];
+        for (let i = 0; i < this.rows; ++i) {
+            const row = this.getRow(i);
+            ret.push(row);
+        }
+        return ret;
+    }
+
+    forceReshape(dims: Dim) {
+        const m = new Matrix(this.Buffer, dims);
+        m.transposed = this.transposed;
+
+        m.fill((i, j) => {
+            if (this.inBounds(i, j)) return this.get(i, j);
+            return 0;
+        });
+
+        this.merge(m);
+    };
+
+    print(digits = 3) {
+        for (let i = 0; i < this.rows; ++i) {
+            for (let j = 0; j < this.cols; ++j) {
+                const x = this.get(i, j).toFixed(digits);
+                process.stdout.write(`${x} `);
+            }
+            process.stdout.write('\n');
+        }
+    }
+
+    equal(m: Matrix<any>) {
+        if (this.rows !== m.rows || this.cols !== m.cols) return false;
+
+        for (let i = 0; i < this.rows; ++i) {
+            for (let j = 0; j < this.cols; ++j) {
+                if (this.get(i, j) !== m.get(i, j)) return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ tests/Matrix.test.ts
@@ -0,0 +1,165 @@
+import Matrix from 'Matrix';
+
+const buildSimpleMatrix = () => Matrix.fromData([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]);
+
+test("Can create Matrix with primitives", () => {
+    const m = buildSimpleMatrix();
+    expect(m).toBeTruthy();
+});
+
+test("Can access data in memory", () => {
+    const m = buildSimpleMatrix();
+
+    expect(m.get(0, 0)).toBe(1);
+    expect(m.get(2, 0)).toBe(7);
+});
+
+test("Can access transposed memory", () => {
+    const m = buildSimpleMatrix();
+
+    m.transpose();
+    expect(m.get(0, 0)).toBe(1);
+    expect(m.get(2, 0)).toBe(3);
+});
+
+test("Can get dimensions of matrix", () => {
+    const m = buildSimpleMatrix();
+    const { rows, cols } = m.dims();
+
+    expect(rows).toBe(3);
+    expect(cols).toBe(3);
+});
+
+test("Can set the value at a specified index", () => {
+    const m = buildSimpleMatrix();
+
+    m.set(0, 1, 3);
+    expect(m.get(0, 1)).toBe(3);
+});
+
+test("Throws error when get out of bounds", () => {
+    const m = buildSimpleMatrix();
+
+    try {
+        m.get(6, 2);
+        fail();
+    } catch(e) {
+        expect(true).toBeTruthy();
+    }
+
+    try {
+        m.get(2, 6);
+        fail();
+    } catch(e) {
+        expect(true).toBeTruthy();
+    }
+});
+
+test("Can get a matrix from another", () => {
+    const m = Matrix.fromData([
+        [0, 1],
+        [2, 3],
+        [4, 5]
+    ]);
+
+    m.transpose();
+    const n = Matrix.fromMatrix(m);
+
+    const e = Matrix.fromData([
+        [0, 2, 4],
+        [1, 3, 5]
+    ]);
+
+    expect(e.equal(n)).toBe(true);
+});
+
+test("Can add a row to a matrix", () => {
+    const m = buildSimpleMatrix();
+
+    m.addRow([1, 2, 3]);
+
+    const { rows, cols } = m.dims();
+
+    expect(rows).toBe(4);
+    expect(cols).toBe(3);
+
+    const e = Matrix.fromData([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 2, 3],
+    ]);
+
+    expect(m.equal(e)).toBe(true);
+});
+
+test("Can add a col to a matrix", () => {
+    const m = buildSimpleMatrix();
+
+    m.addCol([1, 2, 3]);
+
+    const { rows, cols } = m.dims();
+
+    expect(rows).toBe(3);
+    expect(cols).toBe(4);
+
+    const e = Matrix.fromData([
+        [1, 2, 3, 1],
+        [4, 5, 6, 2],
+        [7, 8, 9, 3],
+    ]);
+
+    expect(m.equal(e)).toBe(true);
+});
+
+test("Can get a row", () => {
+    const m = buildSimpleMatrix();
+
+    const row = m.getRow(1);
+
+    expect(row).toEqual([4, 5, 6]);
+});
+
+test("Can get a col", () => {
+    const m = buildSimpleMatrix();
+
+    const col = m.getCol(1);
+
+    expect(col).toEqual([2, 5, 8]);
+});
+
+test("Can force a matrix to resize smaller", () => {
+    const m = buildSimpleMatrix();
+
+    m.forceReshape({rows: 2, cols: 2});
+    expect(Matrix.fromData([
+        [1, 2],
+        [4, 5]
+    ]).equal(m)).toBe(true);
+});
+
+test("Can force a matrix to resize larger", () => {
+    const m = buildSimpleMatrix();
+
+    m.forceReshape({rows: 4, cols: 4});
+    expect(Matrix.fromData([
+        [1, 2, 3, 0],
+        [4, 5, 6, 0],
+        [7, 8, 9, 0],
+        [0, 0, 0, 0]
+    ]).equal(m)).toBe(true);
+});
+
+test("Can build a matrix of zeros", () => {
+    const m = Matrix.Zeros({ rows: 3, cols: 2});
+
+    expect(Matrix.fromData([
+        [0, 0],
+        [0, 0],
+        [0, 0]
+    ]).equal(m)).toBe(true);
+});
