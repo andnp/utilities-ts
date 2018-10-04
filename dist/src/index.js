4da0f91e18e9cc2248db49cf4f3ebc2a07be748a
andy <andnpatterson@gmail.com>
2018-10-04 08:50:25 -0600

    export new utils

--- dist/src/index.js
+++ dist/src/index.js
@@ -4,12 +4,21 @@ function __export(m) {
 }
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./Matrix"));
-const _csv = require("./csv");
-const _files = require("./files");
+__export(require("./tsUtil"));
 const _arrays = require("./arrays");
 const _buffers = require("./buffers");
-exports.csv = _csv;
-exports.files = _files;
+const _csv = require("./csv");
+const _dates = require("./dates");
+const _files = require("./files");
+const _objects = require("./objects");
+const _promise = require("./promise");
+const _time = require("./time");
 exports.arrays = _arrays;
 exports.buffers = _buffers;
+exports.csv = _csv;
+exports.dates = _dates;
+exports.files = _files;
+exports.object = _objects;
+exports.promise = _promise;
+exports.time = _time;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 export * from './Matrix';
+export * from './tsUtil';
 
-import * as _csv from './csv';
-import * as _files from './files';
 import * as _arrays from './arrays';
 import * as _buffers from './buffers';
+import * as _csv from './csv';
+import * as _dates from './dates';
+import * as _files from './files';
+import * as _objects from './objects';
+import * as _promise from './promise';
+import * as _time from './time';
 
-export const csv = _csv;
-export const files = _files;
 export const arrays = _arrays;
 export const buffers = _buffers;
+export const csv = _csv;
+export const dates = _dates;
+export const files = _files;
+export const object = _objects;
+export const promise = _promise;
+export const time = _time;
